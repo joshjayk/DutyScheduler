06052a7ce4de63b21ca55436ae7bf8bb0be6a179
Josh Kim <joshjayk@live.com>
2015-12-29 16:47:51 -0500

    fourth commit

--- DutyScheduler.py
+++ DutyScheduler.py
@@ -14,20 +14,24 @@ def main():
 		file.readline()
 		# The third and fourth line are the month and year.
 		line1 = file.readline()
-		month = line1[7:9]
+		month = line1[8:10]
 		line2 = file.readline()
-		year = line2[6:10]
+		year = line2[7:11]
 		# For each line in the file, if it starts with a '#', skip over it.
-		# Else, split the line into a name and day string, remove new line character
-		# from day strings, turn the day strings into a list of days, and format
+		# Else, split the line into name, day and count strings, remove new line character
+		# from count strings, turn the day strings into a list of days, and format
 		# the list of days into datetime format.
+		# name = Name of each person.
+		# dates = Raw dates listed in the input file.
+		# dayCount = Count of how many weekdays they have been on duty.
+		# endCount = Count of how many weekends they have been on duty.
 		for line in file:
 			if line[0] == "#":
 				continue
 			else:
-				name, dates = line.split(': ')
-				if '\n' in dates:
-					dates = dates[:-1]
+				name, dates, dayCount, endCount = line.split(' : ')
+				if '\n' in count:
+					count = count[:-1]
 				daylist = dates.split()
 				datetimelist = []
 				for day in daylist:
@@ -36,6 +40,7 @@ def main():
 				# Lastly, put the appropriate datetime formatted list into the dictionary
 				# with its respective name.
 				unavailable[name] = datetimelist
+				print(count)
 		print(unavailable)
 
 if __name__ == "__main__":
